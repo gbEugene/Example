8f13f9308a650e0e101e73271a8b30be0902b5a7
gbEugene <tobias.woodcarver@gmail.com>
2022-02-23 22:47:26 +0300

    Example 004 If Else project

---
+++ Example004_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "алина")
+{
+    Console.WriteLine("Привет моя умничка!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
